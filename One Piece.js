a7370a1648fda20e26d0a227696f48e06d39306a
cSOWINO <149458923+cSOWINO@users.noreply.github.com>
2024-05-02 11:40:54 +0530

    Add files via upload

---
+++ One Piece.js
@@ -0,0 +1,11 @@
+document.getElementById("manga6").innerHTML ="<h1><big><b>One Piece</b></big></h1><br />" +
+"<b><i>Japanease Name:</i></b> (ワンピース, Wan Pīsu?) <br><centre>The Great Hito-Tsunagi Treasure</centre> <br/>"+ 
+"<b><i>Genres:</i></b> Action, Adventure, Comedy, Crime, Psychological, Supernatural, Mystery, Fantasy<br/>" +
+"<b><i>Author:</i></b> Eiichiro Oda <br/>" +
+"<b><i>Published By:</i></b>  Shueisha's ''shōnen manga magazine'' ''Weekly Shōnen Jump''<br />" +
+"<b><i>Status:</i></b> Publishing <br/>" +
+"<b><i>Rank:</i></b><b> 1 </b><br/>" +
+"<b><i>Rating:</i></b> 9.14";
+
+document.getElementById("manga6Summary").innerHTML ="<p class='toggle'>Gol D. Roger, a man referred to as the ''Pirate King'' is set to be executed by the World Government. But just before his demise, he confirms the existence of a great treasure, One Piece, located somewhere within the vast ocean known as the Grand Line. Announcing that One Piece can be claimed by anyone worthy enough to reach it, the Pirate King is executed and the Great Age of Pirates begins. Twenty-two years later, a young man by the name of Monkey D. Luffy is ready to embark on his own adventure, searching for One Piece and striving to become the new Pirate King. Armed with just a straw hat, a small boat, and an elastic body, he sets out on a fantastic journey to gather his own crew and a worthy ship that will take them across the Grand Line to claim the greatest status on the high seas.</p> "
++ "<button class='mangaButton'>Toggle</button><br />";
---
+++ manga6.js
@@ -0,0 +1,12 @@
+document.getElementById("manga6").innerHTML ="<h1><big><b>Common for all manga</b></big></h1><br />" +
+"<b><i>Japanease Name:</i></b>Shows name of manga in japanease<br/>"+ 
+"<b><i>Genres:</i></b> Shows manga genre<br/>" +
+"<b><i>Author:</i></b> Shows name of authour who created selected manga<br/>" +
+"<b><i>Published By:</i></b> Shows where manga is published<br />" +
+"<b><i>Status:</i></b> Shows Present status of manga <br/>" +
+"<b><i>Rank:</i></b><b> Shows Rank of manga</b><br/>" +
+"<b><i>Rating:</i></b> Shows Rating for manga";
+
+document.getElementById("manga6Summary").innerHTML ="<p class='toggle'>This article is about the comics or graphic novels created in Japan. For other uses,''Manga''"+
+"Manga (漫画, IPA: [maŋga]) are comics or graphic novels originating from Japan. Most manga conform to a style developed in Japan in the late 19th century, and the form has a long history in earlier Japanese art. The term manga is used in Japan to refer to both comics and cartooning. Outside of Japan, the word is typically used to refer to comics originally published in the country.</p>"+
+"<button class='mangaButton'>Toggle</button><br />";
